adfd02d5d36dfeb177fef6d78b5215f201463fc1
jychuong <jessicaychuong@gmail.com>
2023-11-23 03:24:06 -0800

    added something to console log

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import gsap from 'gsap';
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
 gsap.from('.toggle-bar', { duration: 1.8, x: '150%', ease: 'power1.out', delay: .5, duration: .8 });
-console.log("Portfolio inspired by https://bokoko33.me/ ");
+console.log("Portfolio inspired by https://bokoko33.me/ and shout out to Pandawoan! https://www.youtube.com/@pandawoan/ ");
 
